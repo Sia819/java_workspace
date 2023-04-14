0527eabce300784eb03e175c915acbd217e8b57a
Sia819 <lunasia819@gmail.com>
2023-04-14 16:29:24 +0900

    10장 예제 따라하기 완료

--- Jsp/jwbook/src/main/java/ch09/StudentController.java
+++ Jsp/jwbook/src/main/java/ch09/StudentController.java
@@ -14,41 +14,53 @@ import org.apache.commons.beanutils.BeanUtils;
  * Servlet implementation class StudentController
  */
 @WebServlet("/studentControl")
-public class StudentController extends HttpServlet {
+public class StudentController extends HttpServlet 
+{
 	private static final long serialVersionUID = 1L;
        
 	StudentDAO dao;
-	public void init(ServletConfig config) throws ServletException {
+	public void init(ServletConfig config) throws ServletException 
+	{
 		super.init(config);
 		dao = new StudentDAO();
 	}
 
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
 		request.setCharacterEncoding("utf-8");
 		String action = request.getParameter("action");
 		String view = "";
 		
-		if(action == null) {
+		if(action == null) 
+		{
 			getServletContext().getRequestDispatcher("/studentControl?action=list").forward(request, response);
-		} else {
-			switch(action) {
-			case "list": view = list(request, response);break;
-			case "insert": view = insert(request, response);break;
+		} 
+		else 
+		{
+			switch(action) 
+			{
+				case "list": view = list(request, response); break;
+				case "insert": view = insert(request, response); break;
 			}
 			getServletContext().getRequestDispatcher("/ch09/"+view).forward(request, response);		
 		}
 	}
 	
-	public String list(HttpServletRequest request, HttpServletResponse response) {
+	public String list(HttpServletRequest request, HttpServletResponse response) 
+	{
 		request.setAttribute("students", dao.getAll());
 		return "studentInfo.jsp";		
 	}
 	
-	public String insert(HttpServletRequest request, HttpServletResponse response) {
+	public String insert(HttpServletRequest request, HttpServletResponse response) 
+	{
 		Student s = new Student();
-		try {
+		try 
+		{
 			BeanUtils.populate(s, request.getParameterMap());
-		} catch (Exception e) {
+		}
+		catch (Exception e) 
+		{
 			e.printStackTrace();
 		}
 		dao.insert(s);
